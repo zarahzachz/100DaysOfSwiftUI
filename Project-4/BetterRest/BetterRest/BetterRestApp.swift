0ea814deb84408347bb257a698d2fcae673cb394
zarahzachz <zarahzachz@users.noreply.github.com>
2024-11-30 15:33:29 -0500

    Days 26-28 complete (project 4)

---
+++ Project-4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sarah Sachs on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
